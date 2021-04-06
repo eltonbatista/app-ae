9ef9af275ad45912b827dd062500fdb5585cdebb
eltonbatista <eltonbatista@gmail.com>
2021-04-06 15:21:46 -0300

    Nova Home

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,5 @@
-import { useState } from 'react';
-
 function Home() {
-    return (
-        <div>
-            <h2>Home 2</h2>
-            <Contador />
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
+    return <div> APP AE Tecnologia</div>
 }
 
 export default Home
